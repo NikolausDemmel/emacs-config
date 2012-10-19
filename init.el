223f7e9017d34d3347af64364527e2b68e5e2f51
Nikolaus Demmel <fixed-term.Nikolaus.Demmel@us.bosch.com>
2012-10-18 17:57:14 -0700

    updates

--- init.el
+++ init.el
@@ -13,8 +13,20 @@
 ;; to the rosemacs commands
 (global-set-key "\C-x\C-r" ros-keymap)
 
-(set-default-font "-unknown-DejaVu Sans Mono-normal-normal-normal-*-*-*-*-*-m-0-iso10646-1")
+;;(set-default-font "-unknown-DejaVu Sans Mono-normal-normal-normal-*-*-*-*-*-m-0-iso10646-1")
+(set-default-font "-unknown-Droid Sans Mono-normal-normal-normal-*-13-*-*-*-m-0-iso10646-1")
 
 (setq confirm-kill-emacs 'yes-or-no-p)
 
 (global-set-key [f9] 'recompile)
+(global-set-key [f10] 'compile)
+
+
+
+(require 'magit-svn)
+(global-set-key (kbd "C-x g") 'magit-status)
+
+
+(defun gitg ()
+  (interactive)
+  (shell-command "gitg"))
